f40f7911004a83ef14abf6d7db4b87af8a705a19
DreamHust13 <1506732332@qq.com>
2015-01-28 20:30:40 +0800

    OrQuery：：eval函数解析

--- C++/c++_primer_text_query_CLASS/Query.cpp
+++ C++/c++_primer_text_query_CLASS/Query.cpp
@@ -55,18 +55,18 @@ NotQuery::eval(const TextQuery& text) const
 	return QueryResult(rep(), ret_lines, result.get_file());
 }
 
-// returns the intersection of its operands' result sets
-QueryResult
-AndQuery::eval(const TextQuery& text) const
+// 返回运算对象查询结果set的交集
+QueryResult AndQuery::eval(const TextQuery& text) const
 {
-    // virtual calls through the Query operands to get result sets for the operands
+    // 通过Query成员lhs和rhs进行的虚调用,以获得运算对象的查询结果set
     auto left = lhs.eval(text), right = rhs.eval(text);
 
-	// set to hold the intersection of left and right
+	// 保存left和right交集的set
     auto ret_lines = make_shared<set<line_no>>();  
 
-    // writes the intersection of two ranges to a destination iterator
-    // destination iterator in this call adds elements to ret
+    // 将两个范围的交集写入一个目的迭代器中
+    // 本次调用的目的迭代器向ret添加元素
+	//使用的是标准库算法set_intersection
     set_intersection(left.begin(), left.end(), 
                    right.begin(), right.end(),
                    inserter(*ret_lines, ret_lines->begin()));
